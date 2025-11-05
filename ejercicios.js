afc9bb2b30c331bd784a6295a32e484b4e1643ac
bylev <2009014@upy.edu.mx>
2025-11-05 08:13:07 -0600

    Ejercicio 1 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -15,9 +15,9 @@ function miInformacion() {
   // TODO: Declara las variables nombre, edad y carrera
   // Ejemplo: const nombre = "Juan";
   
-  const nombre = "";
-  const edad = 0;
-  const carrera = "";
+  const nombre = "Michelle";
+  const edad = 24;
+  const carrera = "Desarrollo de Software y Negocios Digitales";
   
   return { nombre, edad, carrera };
 }
