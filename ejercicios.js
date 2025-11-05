64324b5de2c8e31eb2ed2af9706ca6072a735888
bylev <2009014@upy.edu.mx>
2025-11-05 11:41:47 -0600

    feat: Ejercicio 7 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -107,7 +107,8 @@ function evaluarNota(nota) {
  */
 function mayorDeTres(a, b, c) {
   // TODO: Encuentra y retorna el mayor de los tres nÃºmeros
-  return 0;
+  return Math.max(a,b,c)
+
 }
 
 /**
